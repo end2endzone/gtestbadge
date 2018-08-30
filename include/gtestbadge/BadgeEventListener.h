3b8c4922a9f8d006ac86c7666fdbf8c4f787be49
Antoine <end2endzone@gmail.com>
2018-08-29 21:49:22 -0400

    Added API documentation.

--- include/gtestbadge/BadgeEventListener.h
+++ include/gtestbadge/BadgeEventListener.h
@@ -27,24 +27,58 @@ public:
   virtual void OnTestIterationEnd(const UnitTest& unit_test, int iteration);
   virtual void OnTestProgramEnd(const UnitTest& unit_test);
 
-  //
+  ///<summary>Set the output file path used for saving.</summary>
+  ///<param name="iFilename">The output file path used for saving.</param>
   void setOutputFilename(const std::string & iFilename);
+ 
+  ///<summary>Get the output file path used for saving.</summary>
+  ///<returns>Returns the output file path used for saving.</returns>
   const std::string & getOutputFilename() const { return mOutputFilename; }
+ 
+  ///<summary>
+  ///Set the warning ratio.
+  ///The warning ratio defines the number of test failures at which the badge shows a red background instead of an orange background.
+  ///The ratio must be between 0 and 1.
+  ///For instance, set the ratio to 0.1 to show a red background if 10% or more of test fails.
+  ///Set to 0 to disable the 'warning' badge functionality.
+  ///</summary>
+  ///<param name="ratio">The warning ratio from 0 to 1.</param>
   void setWarningRatio(double ratio);
+ 
+  ///<summary>Get the warning ratio.</summary>
+  ///<returns>Returns the warning ratio.</returns>
   const double & getWarningRatio() const { return mWarningRatio; }
-
-  //constants
+ 
+  ///<summary>Default value for warning ratio. See <see cref="setWarningRatio(double)"/> for details. </summary>
   static const double DEFAULT_WARNING_RATIO;
-
+ 
+  ///<summary>Define the list of supported icon.</summary>
   enum SYSTEM_ICON
   {
     ICON_NONE,
     ICON_APPVEYOR,
     ICON_TRAVIS,
   };
+ 
+  ///<summary>Generate a test badge based on the given test results.</summary>
+  ///<param name="iFilename">The file path to save the image.</param>
+  ///<param name="success">The number of successful tests.</param>
+  ///<param name="failures">The number of failed tests.</param>
+  ///<param name="disabled">The number of disabled tests.</param>
+  ///<param name="iIcon">The id of a badge icon.</param>
+  ///<returns>Returns true if the badge was properly saved. Returns false otherwise.</returns>
   static bool generateBadge(const std::string & iFilename, int success, int failures, int disabled, const SYSTEM_ICON & iIcon);
-  static bool generateBadge(const std::string & iFilename, const double & iWarningRatio, int success, int failures, int disabled, const SYSTEM_ICON & iIcon);
-
+ 
+  ///<summary>Generate a test badge based on the given test results.</summary>
+  ///<param name="iFilename">The file path to save the image.</param>
+  ///<param name="success">The number of successful tests.</param>
+  ///<param name="failures">The number of failed tests.</param>
+  ///<param name="disabled">The number of disabled tests.</param>
+  ///<param name="iIcon">The id of a badge icon.</param>
+  ///<param name="iWarningRatio">The warning ratio to use. See <see cref="setWarningRatio(double)"/> for details.</param>
+  ///<returns>Returns true if the badge was properly saved. Returns false otherwise.</returns>
+  static bool generateBadge(const std::string & iFilename, int success, int failures, int disabled, const SYSTEM_ICON & iIcon, const double & iWarningRatio);
+ 
 private:
   std::string mOutputFilename;
   double mWarningRatio;
