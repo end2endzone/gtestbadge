4e846fc5279a628bdf422d2efa2f2e96ddafe375
Antoine <end2endzone@gmail.com>
2018-09-08 16:03:12 -0400

    Created more samples.

--- samples/samples.cpp
+++ samples/samples.cpp
@@ -71,6 +71,36 @@ int main(int argc, char **argv)
     return __LINE__;
   }
 
+  //warning disabled
+  if (!processBadge("sample_warning_disabled.svg", 1, 1))
+  {
+    return __LINE__;
+  }
+
+  //icon appveyor
+  {
+    std::string sample_dir = SAMPLES_DIRECTORY;
+    std::string filepath = sample_dir + ra::filesystem::getPathSeparatorStr() + "sample_icon_appveyor.svg";
+
+    printf("Generating sample badge '%s'\n", filepath.c_str());
+    if ( ! BadgeEventListener::generateBadge(filepath.c_str(), 42, 0, 0, BadgeEventListener::ICON_APPVEYOR, BadgeEventListener::DEFAULT_WARNING_RATIO))
+    {
+      return __LINE__;
+    }
+  }
+
+  //icon travis
+  {
+    std::string sample_dir = SAMPLES_DIRECTORY;
+    std::string filepath = sample_dir + ra::filesystem::getPathSeparatorStr() + "sample_icon_travis.svg";
+
+    printf("Generating sample badge '%s'\n", filepath.c_str());
+    if ( ! BadgeEventListener::generateBadge(filepath.c_str(), 42, 0, 0, BadgeEventListener::ICON_TRAVIS, BadgeEventListener::DEFAULT_WARNING_RATIO))
+    {
+      return __LINE__;
+    }
+  }
+
   //custom
   {
     Badge b;
@@ -78,6 +108,26 @@ int main(int argc, char **argv)
     b.setRightBackgroundColor("#00a2e8");
     b.setRightText("92%");
     std::string filepath = sample_dir + ra::filesystem::getPathSeparatorStr() + "sample_custom_progress.svg";
+    printf("Generating sample badge '%s'\n", filepath.c_str());
+    bool saved = b.save(filepath.c_str());
+    if (!saved)
+    {
+      return __LINE__;
+    }
+  }
+
+  //custom facebook icon
+  {
+    const char * FACEBOOK_ICON = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPCFET0NUWVBFIHN2ZyBQVUJMSUMgIi0vL1czQy8vRFREIFNWRyAxLjAvL0VOIiAiaHR0cDovL3d3dy53My5vcmcvVFIvMjAwMS9SRUMtU1ZHLTIwMDEwOTA0L0RURC9zdmcxMC5kdGQiPgo8c3ZnIHZlcnNpb249IjEuMCIgZmlsbD0iI2ZmZiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4PSIwcHgiIHk9IjBweCIgd2lkdGg9IjE2cHgiIGhlaWdodD0iMTZweCIgdmlld0JveD0iMCAwIDI1MCA1MTAiPgo8cGF0aCBkPSJNMTgwIDUxMkg5OC4wMDhjLTEzLjY5NSAwLTI0LjgzNi0xMS4xNC0yNC44MzYtMjQuODM2VjMwMi4yMjdIMjUuMzM2QzExLjY0IDMwMi4yMjcuNSAyOTEuMDgyLjUgMjc3LjM5di03OS4yNDZjMC0xMy42OTYgMTEuMTQtMjQuODM2IDI0LjgzNi0yNC44MzZoNDcuODM2di0zOS42ODRjMC0zOS4zNDggMTIuMzU1LTcyLjgyNCAzNS43MjYtOTYuODA1QzEzMi4zNzUgMTIuNzMgMTY1LjE4NCAwIDIwMy43NzggMGw2Mi41My4xMDJjMTMuNjcyLjAyMyAyNC43OTQgMTEuMTY0IDI0Ljc5NCAyNC44MzV2NzMuNTc5YzAgMTMuNjk1LTExLjEzNyAyNC44MzYtMjQuODI5IDI0LjgzNmwtNDIuMTAxLjAxNWMtMTIuODQgMC0xNi4xMSAyLjU3NC0xNi44MDkgMy4zNjMtMS4xNTIgMS4zMS0yLjUyMyA1LjAwOC0yLjUyMyAxNS4yMjN2MzEuMzUyaDU4LjI3YzQuMzg2IDAgOC42MzYgMS4wODIgMTIuMjg4IDMuMTIgNy44OCA0LjQwMyAxMi43NzggMTIuNzI3IDEyLjc3OCAyMS43MjNsLS4wMzEgNzkuMjQ3YzAgMTMuNjg3LTExLjE0MSAyNC44MjgtMjQuODM2IDI0LjgyOGgtNTguNDd2MTg0Ljk0MUMyMDQuODQgNTAwLjg2IDE5My42OTYgNTEyIDE4MCA1MTJ6Ii8+Cjwvc3ZnPg==";
+    Badge b;
+    b.setBase64Icon(FACEBOOK_ICON);
+    b.setLeftText("Friends");
+    b.setLeftBackgroundColor("#3b5998");
+    b.setLeftForegroundColor("#fff");
+    b.setRightBackgroundColor("#fff");
+    b.setRightForegroundColor("#000");
+    b.setRightText("357");
+    std::string filepath = sample_dir + ra::filesystem::getPathSeparatorStr() + "sample_custom_facebook.svg";
     printf("Generating sample badge '%s'\n", filepath.c_str());
     bool saved = b.save(filepath.c_str());
     if (!saved)
